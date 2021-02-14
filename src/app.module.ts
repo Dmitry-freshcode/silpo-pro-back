751984a08a3f9a916fe857a1119dd4fbddba0030
Dmitry Ivanov <securiti.zp.ua@gmail.com>
2021-02-14 18:40:31 +0200

    heroku3

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,11 +3,15 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProductsModule } from './products/products.module';
+import { ConfigModule } from '@nestjs/config';
+
+const ENV = process.env.NODE_ENV;
 
 @Module({ 
   imports: [ProductsModule,
+    ConfigModule.forRoot({envFilePath: !ENV ? '.env' : `.env.${ENV}`,isGlobal: true}),
   //   MongooseModule.forRoot(
-  //   'mongodb://localhost/silpoPro',
+  //   process.env.MONGODB_CONNECTION_DEV,
   //   {
   //     useNewUrlParser: true,
   //     useUnifiedTopology: true,
